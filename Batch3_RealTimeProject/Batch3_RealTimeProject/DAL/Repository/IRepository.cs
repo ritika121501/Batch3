329ed079b1784cbec882b3e69c2c306c7c4a10b0
Prashant Jha <jha_prashant@outlook.com>
2024-03-30 05:40:47 +0530

    Pushing repo pattern

---
+++ Batch3_RealTimeProject/Batch3_RealTimeProject/DAL/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Batch3_RealTimeProject.DAL.Repository
+{
+    public interface IGenericRepo<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Update(T objModel);
+        void DeleteById(T obj);
+        void Create(T objModel);
+    }
+}
---
+++ Batch3_RealTimeProject/Batch3_RealTimeProject/DAL/Repository/Repository.cs
@@ -0,0 +1,38 @@
+﻿namespace Batch3_RealTimeProject.DAL.Repository
+{
+    public class GenericRepo<T> : IGenericRepo<T> where T : class
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public GenericRepo(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public void DeleteById(T obj)
+        {
+            _applicationDbContext.Set<T>().Remove(obj);
+            _applicationDbContext.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _applicationDbContext.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _applicationDbContext.Set<T>().Find(id);
+        }
+
+        public void Update(T objModel)
+        {
+            _applicationDbContext.Entry(objModel).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _applicationDbContext.SaveChanges();
+        }
+
+        public void Create(T objModel)
+        {
+            _applicationDbContext.Entry(objModel).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            _applicationDbContext.SaveChanges();
+        }
+    }
+}
