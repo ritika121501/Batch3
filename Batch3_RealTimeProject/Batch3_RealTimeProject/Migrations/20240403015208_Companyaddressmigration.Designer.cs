cbbddfe5581cbe196a3d0c9122685e0bbda4c198
soujanya262 <soujanyap172@gmail.com>
2024-04-02 21:02:27 -0500

    updated model and done migration(Company,Address)

--- Batch3_RealTimeProject/Batch3_RealTimeProject/Migrations/20240403015208_Companyaddressmigration.Designer.cs
+++ Batch3_RealTimeProject/Batch3_RealTimeProject/Migrations/20240403015208_Companyaddressmigration.Designer.cs
@@ -3,6 +3,7 @@ using Batch3_RealTimeProject.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Batch3_RealTimeProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240403015208_Companyaddressmigration")]
+    partial class Companyaddressmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Batch3_RealTimeProject/Batch3_RealTimeProject/Models/Company.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Batch3_RealTimeProject.Models
+{
+    public class Company
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CompanyId { get; set; }
+
+        public string CompanyName { get; set; }
+        [ForeignKey("AddressId")]
+        public int AddressId { get; set; }
+        public virtual Address Address{get;set;}
+
+    }
+}
